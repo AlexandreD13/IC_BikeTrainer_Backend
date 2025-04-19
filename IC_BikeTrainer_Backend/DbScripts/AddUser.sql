d7fa23adbb15f673a4c5b9254bca3878517c859e
Alexandre Desfosses <alexandredesfosses@hotmail.com>
2025-04-19 18:14:45 -0400

    Changed rateLimiter requests limit

--- IC_BikeTrainer_Backend/DbScripts/AddUser.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO UsersTable (Username, Password, Firstname, Lastname, Email, Weight, Height, Role, ProfileCreationDate)
-VALUES 
-    ('johndoe','securepassword123','John','Doe','johndoe@example.com',72.5,180.2,0,CURRENT_TIMESTAMP);
